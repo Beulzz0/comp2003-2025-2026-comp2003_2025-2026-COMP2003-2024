10650b4b20a69e12539024c04e78b7a271831dfe
Beulzz0 <beulah.oniasanmi@plymouth.ac.uk>
2025-12-31 16:09:43 +0000

    feat: add project files and remove nested git repos

---
+++ SourceCode/who_we_are.js
@@ -0,0 +1,11 @@
+function button_home() {
+    window.location.href = "SaltyPadel.html";
+}
+
+function button_contact() {
+    window.location.href = "contact_us.html";
+}
+
+function button_join_our_whatsapp_group() {
+    window.location.href = "contact_us.html";
+}
